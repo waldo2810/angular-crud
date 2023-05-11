4b9dc7ff05a9c7373a729f8003ce723342298203
Waldo Sobrino <waldo2810@gmail.com>
2023-05-10 21:44:06 -0500

    spring security continuation

--- src/main/java/com/angularpractica/server/controller/ServerController.java
+++ src/main/java/com/angularpractica/server/controller/ServerController.java
@@ -20,7 +20,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/server")
 @RequiredArgsConstructor
 public class ServerController {
--- src/main/java/com/angularpractica/server/security/auth/AuthController.java
+++ src/main/java/com/angularpractica/server/security/auth/AuthController.java
@@ -3,10 +3,7 @@ package com.angularpractica.server.security.auth;
 import com.angularpractica.server.security.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/server/auth")
